ad8238c402a9160cbbd2489faae16a9c288f4b91
Haojia Yu <yuhaojia@wirelessprv-10-194-252-158.near.illinois.edu>
2018-10-03 13:52:21 -0500

    file submission version 23

--- mp1/src/http_server.c
+++ mp1/src/http_server.c
@@ -205,7 +205,7 @@ exit(0);
 			  //send(new_fd,RESP_200,strlen(RESP_200),0);
 			  while(num_bytes=fgets(filebuff, MAXIMUMDATA-1,pfile)!=NULL){
 			    //filebuff[strlen(filebuff)]='\0';
-			    send(new_fd,filebuff,MAXIMUMDATA,0);
+			    send(new_fd,filebuff,strlen(filebuff),0);
 		            printf("numof bytes sent:%d\n",strlen(filebuff));
 			    printf("%s\n",filebuff);
 				memset(filebuff,'\0',MAXIMUMDATA);
