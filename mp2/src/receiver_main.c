6ccae42a3bc0a83d8c7c5e5b2b89847e9ea2f7e7
Haojia Yu <yuhaojia@wirelessprv-10-194-35-147.near.illinois.edu>
2018-10-31 18:15:20 -0500

    Initial

--- mp2/src/receiver_main.c
+++ mp2/src/receiver_main.c
@@ -16,26 +16,118 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#define MSS 1000
+#define RWND 20000
+#define DATA 0
+#define SYN 1
+#define SYN_ACK 2
+#define ACK 3
+#define FIN 4
+#define FIN_ACK 5
+#define MAXSEQUENCE 150000
 
+typedef struct{
+    int     data_size;
+    int     seq_num;
+    int   ack_num;
+    int   msg_type; //SYN 0 ACK 1 FIN 2 FINACK 3
+    char  data[MSS];
+}packet_t;
 
+#define CLOSED 0
+#define LISTEN 1
+#define SYN_RCVD 2
+#define ESTABLISHED 3
+#define CLOSE_WAIT 4
+#define LAST_ACK 5
+#define MAXBUFSIZE 10000
+#define HEADERSIZE 16
+
+socklen_t addr_len;
+FILE* fp;
+char buf[sizeof(packet_t)];
 struct sockaddr_in si_me, si_other;
 int s, slen;
+int nextACK = 0;
+char file_buffer[MAXBUFSIZE];
+int bufferindex = 0;
+int  receive_window[RWND];
+packet_t window_buffer[RWND];
+
+
 
 void diep(char *s) {
     perror(s);
     exit(1);
 }
 
+void manage(packet_t pkt){
+  if(pkt.seq_num == nextACK){// in order packet comes
+    int i;
+    for(i =0; i < pkt.data_size;i++){//load data into file buffer
+      file_buffer[bufferindex] = pkt.data[i];
+      bufferindex++;
+      if(bufferindex==MAXBUFSIZE){
+          // write to file
+          fwrite(file_buffer,sizeof(char),MAXBUFSIZE,fp);
+          bufferindex=0;
+      }
+    }
+    nextACK++;//
+///////////////////////////////////////////////////////////////////////////
+    while(receive_window[nextACK] != 0 ){//
+        receive_window[nextACK]=0;//
+        for(i=0;i<window_buffer[nextACK].data_size;i++){//
+            file_buffer[bufferindex] = window_buffer[nextACK].data[i];
+            bufferindex++;
+            if(bufferindex==MAXBUFSIZE){
+                // write to file
+                fwrite(file_buffer,sizeof(char),MAXBUFSIZE,fp);
+                bufferindex=0;
+            }
+        }
+    nextACK++;//
+  }// end for while loop for ......
+  //send ack
+  packet_t ack;
+  ack.msg_type=ACK;
+  ack.ack_num = nextACK;
+  ack.data_size = 0; // data size is 0 since we are sending ack
+  memcpy(buf,&ack,sizeof(packet_t));
+  sendto(s, buf, sizeof(packet_t), 0, (struct sockaddr *) &si_other,addr_len);
+  }// end if for in order packet comes
+  else if(pkt.seq_num > nextACK){ // out of order packet comes send dupack
+    printf("out of order! send dup ack %d",pkt.seq_num);
+    if (receive_window[pkt.seq_num]==0){//
+        receive_window[pkt.seq_num]=1;//
+        memcpy(&window_buffer[pkt.seq_num], &pkt, sizeof(packet_t));//
+    }
+    //send ack
+    packet_t ack;
+    ack.msg_type=ACK;
+    ack.ack_num = nextACK;
+    ack.data_size = 0; // data size is 0 since we are sending ack
+    memcpy(buf,&ack,sizeof(packet_t));
+    sendto(s, buf, sizeof(packet_t), 0, (struct sockaddr *) &si_other,addr_len);
+  }else{ // the missing packet comes
+    packet_t ack;
+    ack.msg_type=ACK;
+    ack.ack_num = nextACK;
+    ack.data_size = 0; // data size is 0 since we are sending ack
+    memcpy(buf,&ack,sizeof(packet_t));
+    //send ack
+    sendto(s, buf, sizeof(packet_t), 0, (struct sockaddr *) &si_other,addr_len);
+  }
+  return;
+}
+
 
 
 void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
-    
+// open up a socket
     slen = sizeof (si_other);
-
-
     if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
         diep("socket");
-
     memset((char *) &si_me, 0, sizeof (si_me));
     si_me.sin_family = AF_INET;
     si_me.sin_port = htons(myUDPport);
@@ -44,12 +136,50 @@ void reliablyReceive(unsigned short int myUDPport, char* destinationFile) {
     if (bind(s, (struct sockaddr*) &si_me, sizeof (si_me)) == -1)
         diep("bind");
 
+//prepare destination file
+    fp = fopen(destinationFile,"wb");
+    // open file for read and write
+    if(fp == NULL){
+      diep("file open");
+      return;
+    }
 
-	/* Now receive data and send acknowledgements */    
+//receive packets
+    int numbytes = 0;
+    addr_len = sizeof si_other;
+    while(1){
+        if((numbytes = recvfrom(s,buf,sizeof(packet_t),0,(struct sockaddr *) &si_other,&addr_len))==-1){
+          diep("receive data");
+          exit(2);
+        }
+        packet_t pkt;
+        memcpy(&pkt,buf,sizeof(packet_t));
+        //determine packet received
+        if(pkt.msg_type == DATA){//it is a regular packet
+            manage(pkt);
+            continue;
+        }else if(pkt.msg_type == FIN){//receive last signal, send lastask close the socket
+              fwrite(file_buffer,sizeof(char),bufferindex,fp);
+              while(1){
+                packet_t pkt;
+                pkt.msg_type = FIN_ACK;
+                pkt.data_size=0;
+                memcpy(buf,&pkt,sizeof(packet_t));
+                if((numbytes = sendto(s, buf, sizeof(packet_t), 0, (struct sockaddr *) &si_other,addr_len))== -1){
+                    diep("send final ack");
+                    exit(2);
+                }
+                break;
+              }
+              break;
+        }//receive signal for final packet sent
 
-    close(s);
-	printf("%s received.", destinationFile);
-    return;
+    }//end of big while
+
+  close(s);
+  fclose(fp);
+    printf("%s received.", destinationFile);
+  return;
 }
 
 /*
--- mp2/src/sender_main.c
+++ mp2/src/sender_main.c
@@ -10,7 +10,11 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <netdb.h>
 #include <stdio.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <sys/file.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
@@ -19,46 +23,336 @@
 #include <signal.h>
 #include <string.h>
 #include <sys/time.h>
+#include <math.h>
 
-struct sockaddr_in si_other;
-int s, slen;
+enum socket_state {CLOSED, SLOW_START, CONGESTION_AVOID, FAST_RECOVERY, FIN_WAIT};
+
+#define MSS 1000
+#define SWND 150
+#define DATA 0
+#define SYN 1
+#define SYN_ACK 2
+#define ACK 3
+#define FIN 4
+#define FIN_ACK 5
+#define MAX_SEQ 150000
+
+#define min(a, b) a < b ? a : b
+#define max(a, b) a > b ? a : b
+
+typedef struct{
+    int     data_size;
+    int     seq_num;
+    int     ack_num;
+    int     msg_type; //SYN 0 ACK 1 FIN 2 FINACK 3
+    char data[MSS];
+}packet;
+
+//struct socketaddr_in bind_addr, receiver_addr;
+struct sockaddr_storage other_addr; // connector's address information
+socklen_t addr_len = sizeof other_addr;
+struct addrinfo hints, *recvinfo, *p;
+
+FILE *fp;
+
+// For congestion control
+double cwnd = 1;
+int ssthread = 8, dupACK = 0;
+int send_base = 0, next_send = 0, file_pt = 0;
+int send_base_acked = 0, global_seq_num = 0;
+
+// Timing related
+int timeout = 60000;
+//estimatedRTT = 20000, deviation = ;
+//uint64_t start_time;
+
+// Tmp parameters or flags
+ssize_t numofbytes;
+int file_read_finished = 0;
+unsigned long long int bytesReadin;
+unsigned long long int num_pkt_sent = 0, num_pkt_received = 0, num_pkt_total = 0;
+
+// Sliding window related
+packet window_buffer[SWND];
+//int64_t sent_time[SWND];
+char buf[sizeof(packet)];
+int soc_state = SLOW_START;
 
 void diep(char *s) {
     perror(s);
     exit(1);
 }
+/*uint64_t timeNow() {
+    struct timeval current;
+    gettimeofday(&current, 0);
+    return (uint64_t)(current.tv_sec * 1000000 + current.tv_usec);
+}
+int64_t procTimeNow() {
+    struct timeval current1;
+    gettimeofday(&current1, 0);
+    uint64_t timeNow = (uint64_t)(current1.tv_sec * 1000000 + current1.tv_usec);
+    return (int64_t) (timeNow - start_time);
+}*/
 
+int createSocket(char * hostname, unsigned short int hostUDPport)
+{
+    int sockfd;
+    int rv;
+    struct addrinfo hints, *servinfo;
+    char portStr[10];
+    memset(&hints, 0, sizeof hints);
+    sprintf(portStr, "%d", hostUDPport);
+    memset(&hints,0,sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_DGRAM;
+    memset(&servinfo,0,sizeof servinfo);
+    if ((rv = getaddrinfo(hostname, portStr, &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("sender: error opening socket");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "sender: failed to bind socket\n");
+        exit(1);
+    }
+    return sockfd;
+}
+
+int fillingWindow(int num_pkt) {
+    if (num_pkt == 0) { return 0; }    
+    int counter = 0, byte_trans = 0;
+    char file_buffer[MSS+1];
+    packet pkt;
+    for (counter = 0; bytesReadin && counter < num_pkt; ++ counter) {
+        /*if (bytesReadin < (unsigned long long int) MSS) {
+            byte_trans_once = (int) bytesReadin;
+        } else {
+            byte_trans_once = MSS;
+        }*/
+        byte_trans = (int) min(bytesReadin, (unsigned long long int) MSS);
+        int read_size = fread (file_buffer, sizeof(char), byte_trans fp);
+        if (read_size > 0) {
+            pkt.data_size = read_size;
+            pkt.seq_num = global_seq_num;
+            pkt.msg_type = DATA;
+            memcpy(pkt.data, &file_buffer, sizeof(char) * byte_trans);
+            memcpy(&window_buffer[file_pt], &pkt, sizeof(packet));
+            file_pt = (file_pt + 1) % SWND;
+            global_seq_num = (global_seq_num + 1) % MAX_SEQ;
+        }
+        bytesReadin -= read_size;
+    }
+
+    //cout << "Filling the window by size " << num_pkt << endl;
+    return counter;
+}
+
+
+int sendPackets (int sockfd) {
+    if (num_pkt_sent == num_pkt_total) {
+        printf("No packet to send");
+        return 0;
+    }
+
+    int last_pk = send_base + (int)cwnd;
+    if (next_send < send_base) {
+        next_send += SWND;
+    }
+    if ((long long int) (num_pkt_total - num_pkt_sent) < last_pk - next_send ) {
+        printf("With %llu remaining. Initially: %d\n", num_pkt_total - num_pkt_sent, last_pk - next_send);
+        last_pk = next_send + num_pkt_total - num_pkt_sent;
+    }
+
+    printf("base: %d, cwnd: %f, base+cwnd: %d, next send: %d\n", send_base, cwnd, last_pk, next_send);
+    if (last_pk <= next_send) {
+        printf("CWND is too small to send.\n");
+        //cout << "CWND is too small to send." << endl;
+    } else {
+        //sendMultiPackets(sockfd, next_send, last_pk);
+        for (int i = next_send; i < last_pk; ++i) {
+            int i_pos_in_swnd = i % SWND;
+            memcpy(buf, &window_buffer[i_pos_in_swnd], sizeof(packet));
+            if((numofbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
+                printf("Fail to send %d pkt in SWND", i_pos_in_swnd);
+                perror("Error: data sending");
+                exit(2);
+            }
+            //cout << "-----------Sending packet " << window_buffer[i_pos_in_swnd].seq_num << "---------------" << endl;
+            //sent_time[i_pos_in_swnd] = procTimeNow();
+        }
+        num_pkt_sent += (last_pk - next_send) % SWND;
+
+    }
+
+    next_send = max(next_send, last_pk) % SWND;
+    //cout << "send_base: " << send_base << " next_send: " << next_send << endl;
+    return 0;
+}
+
+int manage (packet pkt, int sockfd) {
+    //cout << "~~~~ Receiving packet " << pkt.ack_num << " ~~~~" << endl;
+    int ack_pos_in_swnd = pkt.ack_num % SWND;
+    if ((send_base < next_send && (ack_pos_in_swnd > next_send || ack_pos_in_swnd < send_base)) ||
+        (send_base > ack_pos_in_swnd && ack_pos_in_swnd > next_send )) {
+        perror("Out of order ACK");
+        return -1;
+    }
+
+    if ((ack_pos_in_swnd > send_base) ||
+            (next_send < send_base && ack_pos_in_swnd <= next_send)) {
+        num_pkt_received += (pkt.ack_num - send_base) % SWND;
+        //cout << "Received a new ACK with " << pkt.ack_num << endl;
+        fillingWindow ((pkt.ack_num - send_base) % SWND);
+        send_base = ack_pos_in_swnd;
+        // new ACK
+        if (soc_state == FAST_RECOVERY) {
+            cwnd = ssthread * 1.0;
+            dupACK = 0;
+            soc_state = CONGESTION_AVOID;
+        }
+
+        //cout << "Total Received pkt: " << num_pkt_received << endl;
+        switch (soc_state) {
+            case SLOW_START:
+                cwnd = (cwnd + 1 < SWND) ? cwnd + 1 : SWND - 1;
+                dupACK = 0;
+                break;
+            case CONGESTION_AVOID:
+                cwnd = (cwnd + 1.0/cwnd < SWND) ? cwnd + 1.0 / cwnd : SWND - 1;
+                dupACK = 0;
+                break;
+            case FAST_RECOVERY:
+                cwnd = ssthread;
+                dupACK = 0;
+                soc_state = CONGESTION_AVOID;
+                break;
+            default:
+                perror("Wrong socket status");
+                return -1;
+        }
+    } else if (ack_pos_in_swnd == send_base) { // dup ACK
+        //printSWNDseq();
+        if (soc_state == SLOW_START || soc_state == CONGESTION_AVOID) {
+            ++dupACK;
+            // dupACK to 3
+            if (dupACK >= 3) {
+                ssthread = (int) cwnd / 2;
+                cwnd = ssthread + 3.0;
+                //sendSinglePacket(sockfd, send_base);
+
+                memcpy(buf, &window_buffer[send_base], sizeof(packet));
+                if((numofbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
+                    printf("Fail to send %d pkt", send_base);
+                    perror("Error: data sending (single)");
+                    exit(2);
+                }
+                //cout << "-----------Sending single packet " << window_buffer[pt].seq_num << "---------------" << endl;
+                //sent_time[pt] = procTimeNow();
+
+                soc_state = FAST_RECOVERY;
+            }
+        } else { cwnd = (cwnd + 1 < SWND) ? cwnd + 1 : SWND - 1; }
+    } else {
+        perror("Invalid ACK packet");
+    }
+
+    // cwnd > ssthread
+    if (soc_state == SLOW_START && cwnd > ssthread) {
+        soc_state = CONGESTION_AVOID;
+    }
+
+//    updateTimeout(sent_time[ack_pos_in_swnd]);
+    sendPackets(sockfd);
+
+    return 0;
+}
 
 void reliablyTransfer(char* hostname, unsigned short int hostUDPport, char* filename, unsigned long long int bytesToTransfer) {
     //Open the file
-    FILE *fp;
+    bytesReadin = bytesToTransfer;
+    //FILE *fp;
     fp = fopen(filename, "rb");
     if (fp == NULL) {
         printf("Could not open file to send.");
         exit(1);
     }
+    //num_pkt_total = (unsigned long long int) ceil(bytesToRead * 1.0 / MSS);
+    //cout << num_pkt_total << endl;
 
-	/* Determine how many bytes to transfer */
-
-    slen = sizeof (si_other);
-
-    if ((s = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-        diep("socket");
-
-    memset((char *) &si_other, 0, sizeof (si_other));
-    si_other.sin_family = AF_INET;
-    si_other.sin_port = htons(hostUDPport);
-    if (inet_aton(hostname, &si_other.sin_addr) == 0) {
-        fprintf(stderr, "inet_aton() failed\n");
-        exit(1);
+    if (fillingWindow(SWND) == -1) {
+        perror("Error during filling window");
     }
 
+    int sockfd = createSocket(hostname, hostUDPport);
+    /*struct timeval current;
+    gettimeofday(&current, 0);
+    start_time = (uint64_t)(current.tv_sec * 1000000 + current.tv_usec);*/
 
-	/* Send data and receive acknowledgements on s*/
+    struct timeval tv;
+    tv.tv_sec = 0; tv.tv_usec = timeout;
+    if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
+        perror("sender: setsockopt");
+    }
+    printf("Setting timeout to %d\n", timeout);
 
-    printf("Closing the socket\n");
-    close(s);
-    return;
+    sendPackets(sockfd);
+    while (num_pkt_sent < num_pkt_total ||
+           num_pkt_received < num_pkt_sent) {
+        // Wait for ack
+        if((numofbytes = recvfrom(sockfd, buf, sizeof(packet), 0, NULL, NULL)) == -1) {
+            if (errno != EAGAIN || errno != EWOULDBLOCK) {
+                perror("can not receive main ack");
+                exit(2);
+            }
+            // Timeout
+            memcpy(buf, &window_buffer[send_base], sizeof(packet));
+            if((numofbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen))== -1){
+                perror("Error: timeout sending");
+                printf("Fail to send %d pkt in SWND", send_base);
+                exit(2);
+            }
+            printf("---------- Timeout resending packet %d ----------\n", window_buffer[send_base].seq_num);
+            //printSentTime();
+            ssthread = cwnd/2; cwnd = 1.0; dupACK = 0;
+
+        }
+
+        packet pkt;
+        memcpy(&pkt, buf, sizeof(packet));
+        if(pkt.msg_type == ACK) {
+            manage(pkt, sockfd);
+            continue;
+        }
+    }
+
+    fclose(fp);
+
+    packet pktend;
+    while (1) {
+            pktend.msg_type = FIN;
+            pktend.data_size=0;
+            memcpy(buf, &pktend, sizeof(packet));
+            numofbytes = sendto(sockfd, buf, sizeof(packet), 0, p->ai_addr, p->ai_addrlen);
+            packet ack;
+            numofbytes = recvfrom(sockfd, buf, sizeof(packet), 0, (struct sockaddr *) &other_addr, &addr_len);
+            memcpy(&ack, buf, sizeof(packet));
+            if (ack.msg_type == FIN_ACK) {
+                printf("Receive the FIN_ACK.\n");
+                soc_state = FIN_WAIT;
+                break;
+            }
+        }
 
 }
 
